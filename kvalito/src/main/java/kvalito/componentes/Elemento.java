b78a53870f85a3c328c51275486d8966e3aabcd8
rbosisio <rod.bosisio@gmail.com>
2015-02-19 15:55:30 -0200

    Atualizando javadocs

--- kvalito/src/main/java/kvalito/componentes/Elemento.java
+++ kvalito/src/main/java/kvalito/componentes/Elemento.java
@@ -13,8 +13,9 @@ import de.svenjacobs.loremipsum.LoremIpsum;
 public class Elemento extends ElementoCore {
 	private AcoesTeclado teclado;
 
-	public Elemento() throws Exception { }
-	
+	public Elemento() throws Exception {
+	}
+
 	public Elemento(Localizador localizadorDoElemento) throws Exception {
 		super.carregarElemento(localizadorDoElemento);
 	}
@@ -28,81 +29,135 @@ public class Elemento extends ElementoCore {
 		Localizador localizador = new Localizador(expressaoLocalizacao);
 		super.carregarElemento(localizador);
 	}
-	
+
 	public Elemento(WebElement webElement) {
 		this.elemento = webElement;
 	}
-	
+
 	public WebElement webElement() {
 		return elemento;
 	}
-	
+
 	public AcoesTeclado acoesTeclado() {
 		if (this.teclado == null) {
 			this.teclado = new AcoesTeclado(this.elemento);
 		}
 		return this.teclado;
 	}
-	
+
+	/**
+	 * Verifica se o elemento está visível. <br>
+	 * 
+	 */
+
 	public boolean estaVisivel() throws Exception {
 		return elemento.isDisplayed();
 	}
-		
+
+	/**
+	 * Verifica se o elemento está habilitado. <br>
+	 * 
+	 */
 	public boolean estaHabilitado() throws Exception {
 		return elemento.isEnabled();
 	}
-	
+
+	/**
+	 * Verifica se o elemento(input/checkbox/radio button) está selecionado. <br>
+	 * 
+	 */
 	public boolean estaSelecionado() {
 		return elemento.isSelected();
 	}
-	
-	public void selecionar() {
-		if (!this.estaSelecionado()){
-			elemento.click();
-		}	
-	}
-	
+
 	/**
-	 * Obtém o valor de um atributo do elemento HTML <br>
+	 * Seleciona o elemento(input/checkbox/radio button). <br>
+	 * 
+	 */
+	public void selecionar() {
+		if (!this.estaSelecionado()) {
+			elemento.click();
+		}
+	}
+
+	/**
+	 * Retorna o valor de um atributo do elemento HTML <br>
+	 * 
+	 * @param nomeDoAtributo
 	 */
 	public String valorAtributo(String nomeDoAtributo) {
 		return elemento.getAttribute(nomeDoAtributo);
 	}
-	
+
 	/**
-	 * Obtém o valor (atributo VALUE) de um elemento HTML <br>
+	 * Retorna o valor (atributo VALUE) de um elemento HTML <br>
+	 * 
 	 */
 	public String valor() {
 		return valorAtributo("value");
 	}
-	
+
+	/**
+	 * Retorna o texto de um elemento HTML <br>
+	 * 
+	 */
 	public String getConteudo() {
 		return elemento.getText();
 	}
-	
+
+	/**
+	 * Retorna o texto de um elemento HTML. <br>
+	 * 
+	 */
 	public String conteudo() {
 		return elemento.getText();
 	}
-	
+
+	/**
+	 * Verifica a existencia do texto no conteúdo do elemento. <br>
+	 * 
+	 * @param valorVerificarExistencia
+	 * 
+	 */
 	public boolean conteudoContem(String valorVerificarExistencia) {
 		return elemento.getText().contains(valorVerificarExistencia);
 	}
-	
+
 	public boolean estaPreenchido() {
 		return possuiConteudo();
 	}
-		
+
+	/**
+	 * Preenche o input com o texto passado por parâmetro. <br>
+	 * 
+	 * @param texto
+	 * 
+	 */
 	public void preencherCom(String texto) {
 		this.elemento.sendKeys(texto);
 	}
-	
+
+	/**
+	 * Preenche o input com um texto aleatório com a quantidade de palavras
+	 * passadas por parâmetro. <br>
+	 * 
+	 * @param quantidadePalavras
+	 * 
+	 */
 	public String preencherComTextoAleatorio(int quantidadePalavras) {
 		LoremIpsum geradorLoremIpsum = new LoremIpsum();
 		String texto = geradorLoremIpsum.getWords(quantidadePalavras);
 		this.preencherCom(texto);
 		return texto;
 	}
-	
+
+	/**
+	 * Preenche o input com um caracteres alfanuméricos aleatórios. <br>
+	 * 
+	 * @param quantidadeCaracteres
+	 * 
+	 */
+
 	public String preencherComCaracteresAleatorios(int quantidadeCaracteres) {
 		String caracteres = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890";
 		Random aleatorio = new Random();
@@ -113,44 +168,51 @@ public class Elemento extends ElementoCore {
 		this.preencherCom(texto);
 		return texto;
 	}
-	
+
+	/**
+	 * Preenche o input com um números aleatórios. <br>
+	 * 
+	 * @param quantidadeNumeros
+	 */
+
 	public String preencherComNumerosAleatorios(int quantidadeNumeros) {
 		Random rand = new Random();
 		int min = 0;
 		int max = 9;
 		String numeroGerado = "";
 		for (int i = 0; i < quantidadeNumeros; i++) {
-			numeroGerado += rand.nextInt((max  - min) + 1) + min;
+			numeroGerado += rand.nextInt((max - min) + 1) + min;
 		}
 		this.preencherCom(numeroGerado);
 		return numeroGerado.toString();
 	}
-		
+
+	/**
+	 * Limpa o conteúdo de um input. <br>
+	 * 
+	 */
 	public void limpar() {
 		this.elemento.clear();
 	}
-	
+
 	@Override
 	protected String getNomeTag() {
 		return "generico";
 	}
-	
+
 	/**
-	 * Retorna a posição (x, y) do elemento na página. 
-	 * @return
+	 * Retorna a posição (x, y) do elemento na página. <br>
+	 * 
 	 */
-	public Point posicao() {		
+	public Point posicao() {
 		return elemento.getLocation();
 	}
 
-	
 	/**
-	 * Obtém o valor (atributo VALUE) do CSS de um elemento <br>
+	 * Retorna o valor (atributo VALUE) do CSS de um elemento <br>
 	 */
 	public String valorAtributoCss(String nomeDoAtributo) {
 		return elemento.getCssValue(nomeDoAtributo);
 	}
-	
 
-	
 }
