dfdab2fc5a27b32e1de817a15577f4630646808d
yuriluz <yuri.almeida@infoglobo.com.br>
2015-06-29 15:25:14 -0300

    Correção do preencherSimulandoDigitacao

--- kvalito/src/test/java/testes/webdriver/TestesAlertaChrome.java
+++ kvalito/src/test/java/testes/webdriver/TestesAlertaChrome.java
@@ -10,72 +10,70 @@ import kvalito.core.Pagina;
 
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 public class TestesAlertaChrome extends Pagina {
-	private String urlPagina;
+    private String urlPagina;
 
-	@Before
-	public void iniciarTestes() throws Exception {
-		executarTesteNo(NavegadorUtilizado.CHROME);
-		urlPagina = Configuracoes.getConfiguracaoPagina("pagina-app-web-qatestengine");
-		abrirUrl(urlPagina);
-	}
+    @Before
+    public void iniciarTestes() throws Exception {
+	executarTesteNo(NavegadorUtilizado.CHROME);
+	urlPagina = Configuracoes.getConfiguracaoPagina("pagina-app-web-qatestengine");
+	abrirUrl(urlPagina);
+    }
 
-	@After
-	public void finalizarTeste() throws Exception {
-		fechar();
-	}
-	
-	@Test
-	public void verificarMensagemAlerta() throws Exception {
-		Elemento botao = localizarElemento("alert-lancar");
-		botao.clicar();
-		
-		String mensagemAlerta = obterTextoAlerta();			
-		String mensagemEsperada = "Exibiu alerta";
-		assertEquals(mensagemEsperada, mensagemAlerta);
-		
-		esperarCarregamentoPor(2000);
-		
-		
-		aceitarAlerta();
-		
-		esperarCarregamentoPor(3000);
-	}
-	
-	@Test
-	public void verificarMensagemAlerta2() throws Exception {
-		Elemento botao = localizarElemento("alert-lancar");
-		botao.clicar();
-		
-		String mensagemAlerta = obterTextoAlerta();			
-		String mensagemEsperada = "Exibiu alerta";
-		assertEquals(mensagemEsperada, mensagemAlerta);
-		
-		esperarCarregamentoPor(2000);
-		
-		aceitarAlerta();
-		
-		esperarCarregamentoPor(3000);
-	}
-	
-	@Test
-	public void localizarElementoPorCssSelector() throws Exception {
-		String resultadoEsperado = "Por CSS-SELECTOR";
-		String textoElemento = localizarElemento("cssselector", "#todos-find-elements-by-cssselector").getConteudo();
-		assertEquals(resultadoEsperado, textoElemento);
-	}
+    @After
+    public void finalizarTeste() throws Exception {
+	fechar();
+    }
 
-	@Test
-	public void executarMouseOver() throws Exception {
-		Elemento divPassarMousePorCima = localizarElemento("mouse-over-passe-mouse");
-		Elemento divQueSeraExibido = localizarElemento("mouse-over-ira-aparecer");
+    @Test
+    public void verificarMensagemAlerta() throws Exception {
+	Elemento botao = localizarElemento("alert-lancar");
+	botao.clicar();
 
-		assertFalse(divQueSeraExibido.estaVisivel());
-		divPassarMousePorCima.passarMouseSobre();
-		assertTrue(divQueSeraExibido.estaVisivel());
-	}
-	
+	String mensagemAlerta = obterTextoAlerta();
+	String mensagemEsperada = "Exibiu alerta";
+	assertEquals(mensagemEsperada, mensagemAlerta);
+
+	esperarCarregamentoPor(2000);
+
+	aceitarAlerta();
+
+	esperarCarregamentoPor(3000);
+    }
+
+    @Test
+    public void verificarMensagemAlerta2() throws Exception {
+	Elemento botao = localizarElemento("alert-lancar");
+	botao.clicar();
+
+	String mensagemAlerta = obterTextoAlerta();
+	String mensagemEsperada = "Exibiu alerta";
+	assertEquals(mensagemEsperada, mensagemAlerta);
+
+	esperarCarregamentoPor(2000);
+
+	aceitarAlerta();
+
+	esperarCarregamentoPor(3000);
+    }
+
+    @Test
+    public void localizarElementoPorCssSelector() throws Exception {
+	String resultadoEsperado = "Por CSS-SELECTOR";
+	String textoElemento = localizarElemento("cssselector", "#todos-find-elements-by-cssselector").getConteudo();
+	assertEquals(resultadoEsperado, textoElemento);
+    }
+
+    @Test
+    public void executarMouseOver() throws Exception {
+	Elemento divPassarMousePorCima = localizarElemento("mouse-over-passe-mouse");
+	Elemento divQueSeraExibido = localizarElemento("mouse-over-ira-aparecer");
+
+	assertFalse(divQueSeraExibido.estaVisivel());
+	divPassarMousePorCima.passarMouseSobre();
+	assertTrue(divQueSeraExibido.estaVisivel());
+    }
+
 }
