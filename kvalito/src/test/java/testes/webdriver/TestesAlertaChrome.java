c3dfd479360c26e9e7908d36c93b794b3375ad2a
yuriluz <yuri.almeida@infoglobo.com.br>
2015-06-29 14:45:19 -0300

    Teste execução no Jenkins

--- kvalito/src/test/java/testes/webdriver/TestesAlertaChrome.java
+++ kvalito/src/test/java/testes/webdriver/TestesAlertaChrome.java
@@ -1,6 +1,8 @@
 package testes.webdriver;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import kvalito.componentes.Elemento;
 import kvalito.core.Configuracoes;
 import kvalito.core.NavegadorUtilizado;
@@ -35,6 +37,45 @@ public class TestesAlertaChrome extends Pagina {
 		String mensagemEsperada = "Exibiu alerta";
 		assertEquals(mensagemEsperada, mensagemAlerta);
 		
+		esperarCarregamentoPor(2000);
+		
+		
 		aceitarAlerta();
+		
+		esperarCarregamentoPor(3000);
 	}
+	
+	@Test
+	public void verificarMensagemAlerta2() throws Exception {
+		Elemento botao = localizarElemento("alert-lancar");
+		botao.clicar();
+		
+		String mensagemAlerta = obterTextoAlerta();			
+		String mensagemEsperada = "Exibiu alerta";
+		assertEquals(mensagemEsperada, mensagemAlerta);
+		
+		esperarCarregamentoPor(2000);
+		
+		aceitarAlerta();
+		
+		esperarCarregamentoPor(3000);
+	}
+	
+	@Test
+	public void localizarElementoPorCssSelector() throws Exception {
+		String resultadoEsperado = "Por CSS-SELECTOR";
+		String textoElemento = localizarElemento("cssselector", "#todos-find-elements-by-cssselector").getConteudo();
+		assertEquals(resultadoEsperado, textoElemento);
+	}
+
+	@Test
+	public void executarMouseOver() throws Exception {
+		Elemento divPassarMousePorCima = localizarElemento("mouse-over-passe-mouse");
+		Elemento divQueSeraExibido = localizarElemento("mouse-over-ira-aparecer");
+
+		assertFalse(divQueSeraExibido.estaVisivel());
+		divPassarMousePorCima.passarMouseSobre();
+		assertTrue(divQueSeraExibido.estaVisivel());
+	}
+	
 }
--- kvalito/src/test/java/testes/webdriver/TestesAlertaFirefox.java
+++ kvalito/src/test/java/testes/webdriver/TestesAlertaFirefox.java
@@ -1,6 +1,8 @@
 package testes.webdriver;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import kvalito.componentes.Elemento;
 import kvalito.core.Configuracoes;
 import kvalito.core.NavegadorUtilizado;
@@ -26,7 +28,7 @@ public class TestesAlertaFirefox extends Pagina {
 		fechar();
 	}
 	
-	@Ignore
+	
 	@Test
 	public void verificarMensagemAlerta() throws Exception {
 		Elemento botao = localizarElemento("alert-lancar");
@@ -36,6 +38,46 @@ public class TestesAlertaFirefox extends Pagina {
 		String mensagemEsperada = "Exibiu alerta";
 		assertEquals(mensagemEsperada, mensagemAlerta);
 		
+		esperarCarregamentoPor(2000);
+		
+		
 		aceitarAlerta();
+		
+		esperarCarregamentoPor(3000);
 	}
+	
+	@Test
+	public void verificarMensagemAlerta2() throws Exception {
+		Elemento botao = localizarElemento("alert-lancar");
+		botao.clicar();
+		
+		String mensagemAlerta = obterTextoAlerta();			
+		String mensagemEsperada = "Exibiu alerta";
+		assertEquals(mensagemEsperada, mensagemAlerta);
+		
+		esperarCarregamentoPor(2000);
+		
+		aceitarAlerta();
+		
+		esperarCarregamentoPor(3000);
+	}
+	
+	@Test
+	public void localizarElementoPorCssSelector() throws Exception {
+		String resultadoEsperado = "Por CSS-SELECTOR";
+		String textoElemento = localizarElemento("cssselector", "#todos-find-elements-by-cssselector").getConteudo();
+		assertEquals(resultadoEsperado, textoElemento);
+	}
+
+	
+	@Test
+	public void executarMouseOver() throws Exception {
+		Elemento divPassarMousePorCima = localizarElemento("mouse-over-passe-mouse");
+		Elemento divQueSeraExibido = localizarElemento("mouse-over-ira-aparecer");
+
+		assertFalse(divQueSeraExibido.estaVisivel());
+		divPassarMousePorCima.passarMouseSobre();
+		assertTrue(divQueSeraExibido.estaVisivel());
+	}
+	
 }
